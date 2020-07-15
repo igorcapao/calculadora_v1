a733248d4d2baa9c411c1167c0b7d87509479c2a
Igor Capão <9020348+igorcapao@users.noreply.github.com>
2020-07-15 14:26:08 -0300

    initial commit

--- calculadora_v1.py
+++ calculadora_v1.py
@@ -1,10 +1,5 @@
 # Calculadora em Python
 
-# Desenvolva uma calculadora em Python com tudo que você aprendeu nos capítulos 2 e 3. 
-# A solução será apresentada no próximo capítulo!
-# Assista o vídeo com a execução do programa!
-
-
 
 def soma(num1, num2):
     """
